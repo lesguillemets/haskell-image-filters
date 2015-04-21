fdff69a5e8b3f4f5c027491b3288df6595035bb5
lesguillemets <lesguillemets@users.noreply.github.com>
2015-04-21 16:20:58 +0900

    initial commit

---
+++ assets/download.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# https://www.flickr.com/photos/wefi_official/12930124385 by Anne Worner, CC-BY-SA.
+wget "https://farm4.staticflickr.com/3761/12930124385_0f6b3ed544_z_d.jpg" -O "room.jpg"
+
+# https://www.flickr.com/photos/pokerbrit/15674868358/ by Steven Wilson, CC-BY.
+wget "https://farm8.staticflickr.com/7527/15674868358_c89c696949_z_d.jpg" -O "tiger.jpg"
+
